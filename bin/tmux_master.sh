75cd7793db5e1afe5e3386e42c04639450517732
Florian Mayer <fmayer@google.com>
2018-06-01 14:14:56 +0100

    allow exit

--- bin/tmux_master.sh
+++ bin/tmux_master.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 NOW=$(date -u +"%Y-%m-%dT%H%M%SZ")
+
+function finish {
+tmux kill-window -t $NOW
+tmux kill-session -t $NOW;
+}
+trap finish EXIT
+
 tmux new-session -d -s main
 tmux new-session -d -s $NOW -t main
 tmux new-window -t $NOW -n $NOW
 tmux attach-session -t $NOW
-tmux kill-window -t $NOW
-tmux kill-session -t $NOW;
